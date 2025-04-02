8a05e0096e88452bfc89cab5b4863fc55153af04
Gustavo-Blois <gustavobloisrr@gmail.com>
2025-04-02 17:20:28 -0300

    Cilindro finalizado

--- src/cilindro.rs
+++ src/cilindro.rs
@@ -1,45 +1,45 @@
 const PI: f32 = 3.141592;
 
-fn coordenada_cilindro(angulo:f32,altura:f32,raio:f32) -> [f32;3] {
-    let x = raio*angulo.cos();
-    let y = raio*angulo.sin();
+fn coordenada_cilindro(angulo: f32, altura: f32, raio: f32) -> [f32; 3] {
+    let x = raio * angulo.cos();
+    let y = raio * angulo.sin();
     let z = altura;
-    [x,y,z]
+    [x, y, z]
 }
 
 //Por padrão, os cilindros serão prismas de base 40
-pub fn cria_cilindro(radius:f32,height:f32) -> Vec<[f32;3]> {
-    let mut vertices: Vec<[f32;3]> = Vec::new();
-    let sector_count:f32 = 40.0;
-    let sector_step:f32 = 2.0*PI/sector_count;
-    let stack_count:f32 = 40.0; 
-    let stack_step:f32 = height/stack_count;
+pub fn cria_cilindro(radius: f32, height: f32) -> Vec<[f32; 3]> {
+    let mut vertices: Vec<[f32; 3]> = Vec::new();
+    let sector_count: f32 = 40.0;
+    let sector_step: f32 = 2.0 * PI / sector_count;
+    let stack_count: f32 = 40.0;
+    let stack_step: f32 = height / stack_count;
 
     for j in 0..(stack_count as i32) {
         for i in 0..(sector_count as i32) {
-            let current_sector = (i as f32)*sector_step;
-            let current_stack = (j as f32)*stack_step;
+            let current_sector = (i as f32) * sector_step;
+            let current_stack = (j as f32) * stack_step;
             let next_sector = {
-                if ((i as f32)+1.0) == sector_count{
-                PI*2.0
+                if ((i as f32) + 1.0) == sector_count {
+                    PI * 2.0
                 } else {
-                ((i as f32)+1.0)*sector_step
+                    ((i as f32) + 1.0) * sector_step
                 }
             };
 
             let next_stack = {
-                if ((j as f32)+1.0) == stack_count {
+                if ((j as f32) + 1.0) == stack_count {
                     height
                 } else {
-                    ((j as f32)+1.0)*stack_step
+                    ((j as f32) + 1.0) * stack_step
                 }
             };
-            
-            let p0 = coordenada_cilindro(current_sector,current_stack,radius);
-            let p1 = coordenada_cilindro(current_sector,next_stack,radius);
-            let p2 = coordenada_cilindro(next_sector,current_stack,radius);
-            let p3 = coordenada_cilindro(next_sector,next_stack,radius);
-            
+
+            let p0 = coordenada_cilindro(current_sector, current_stack, radius);
+            let p1 = coordenada_cilindro(current_sector, next_stack, radius);
+            let p2 = coordenada_cilindro(next_sector, current_stack, radius);
+            let p3 = coordenada_cilindro(next_sector, next_stack, radius);
+
             vertices.push(p0);
             vertices.push(p2);
             vertices.push(p1);
@@ -48,18 +48,20 @@ pub fn cria_cilindro(radius:f32,height:f32) -> Vec<[f32;3]> {
             vertices.push(p1);
             vertices.push(p2);
 
-            if current_stack == 0.0{
+            if current_stack == 0.0 {
                 vertices.push(p0);
                 vertices.push(p2);
-                vertices.push(coordenada_cilindro(0.0,current_stack,0.0));
+                vertices.push(coordenada_cilindro(0.0, current_stack, 0.0));
             }
             if next_stack == height {
                 vertices.push(p1);
                 vertices.push(p3);
-                vertices.push(coordenada_cilindro(0.0,next_stack,0.0));
+                vertices.push(coordenada_cilindro(0.0, next_stack, 0.0));
             }
         }
-        
+    }
+    for vertice in &vertices {
+        println!("{:?}", vertice)
     }
     vertices
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate glfw;
-use glfw::{Action, Context, Key};
 use gl33::*;
-use std::ffi::{c_float,CString};
+use glfw::{Action, Context, Key};
+use rand::prelude::*;
+use std::ffi::{CString, c_float};
 mod cilindro;
 use cilindro::cria_cilindro;
 fn main() {
@@ -10,97 +11,95 @@ fn main() {
 
     // Cria uma janela glfw
     let (mut window, events) = glfw
-        .create_window(800,600,"Uma janela",glfw::WindowMode::Windowed)
+        .create_window(800, 600, "Uma janela", glfw::WindowMode::Windowed)
         .expect("Falha em criar uma janela glfw");
     window.make_current();
     window.set_key_polling(true);
     // Dado um contexto (vindo do glfw) e um processo, do opengl, obtém o endereço do processo e
     // carrega o opengl com esse endereço
-    let gl = unsafe{
-        GlFns::load_from(&|p|{
-                glfw.get_proc_address_raw(&glfw::string_from_c_str(p as *const _)) as *const _
+    let gl = unsafe {
+        GlFns::load_from(&|p| {
+            glfw.get_proc_address_raw(&glfw::string_from_c_str(p as *const _)) as *const _
         })
         .unwrap()
     };
-   
+
     // define o vertex array object
     unsafe {
         let mut vao = 0;
         gl.GenVertexArrays(1, &mut vao);
         //o vertex array object não pode ser zero depois dessa operação
-        assert_ne!(vao,0);
+        assert_ne!(vao, 0);
 
-    //define o vertex buffer object
+        //define o vertex buffer object
 
         let mut vbo = 0;
         gl.GenBuffers(1, &mut vbo);
-        assert_ne!(vbo,0);
-    // Vincula o vbo
-        gl.BindBuffer(GL_ARRAY_BUFFER,vbo);
+        assert_ne!(vbo, 0);
+        // Vincula o vbo
+        gl.BindBuffer(GL_ARRAY_BUFFER, vbo);
 
-    // Vertices:
-    let vertices_cilindro = cria_cilindro(0.1,0.9);
-    let n_vertices_cilindro = vertices_cilindro.len();
-    // Envia nossos vértices pro array buffer
-        gl.BufferData(GL_ARRAY_BUFFER,
-            size_of_val(&vertices_cilindro) as isize,
+        // Vertices:
+        let vertices_cilindro = cria_cilindro(0.1, 0.9);
+        let n_vertices_cilindro = vertices_cilindro.len();
+        // Envia nossos vértices pro array buffer
+        gl.BufferData(
+            GL_ARRAY_BUFFER,
+            (vertices_cilindro.len() * std::mem::size_of::<[f32; 3]>()) as isize,
             vertices_cilindro.as_ptr().cast(),
             GL_DYNAMIC_DRAW,
         );
 
-    //Criação do vertex shader program object
+        //Criação do vertex shader program object
         let vertex_shader = gl.CreateShader(GL_VERTEX_SHADER);
-        assert_ne!(vertex_shader,0);
+        assert_ne!(vertex_shader, 0);
         const VERT_SHADER: &str = r#"#version 330 core
         attribute vec3 position;
         uniform mat4 mat_transformation;
         void main() {
-        gl_Position = vec4(position, 1.0);
+        gl_Position = mat_transformation*vec4(position, 1.0);
         }"#;
-    // envia o código fonte do shader para o nosso vspo
-    
+        // envia o código fonte do shader para o nosso vspo
+
         gl.ShaderSource(
             vertex_shader,
             1, // numero de elementos dos vetores string e tamanho de string. Eu não sei o porquê
-               // disso, mas está na documentação https://registry.khronos.org/OpenGL-Refpages/gl4/html/glShaderSource.xhtml
+            // disso, mas está na documentação https://registry.khronos.org/OpenGL-Refpages/gl4/html/glShaderSource.xhtml
             &(VERT_SHADER.as_bytes().as_ptr().cast()), // a string do código fonte
-            &(VERT_SHADER.len().try_into().unwrap()), // o tamanho da string
+            &(VERT_SHADER.len().try_into().unwrap()),  // o tamanho da string
         );
-    // Compila o vertex shader
+        // Compila o vertex shader
         gl.CompileShader(vertex_shader);
-    
-    // Verifica se a compilação deu certo
-    
+
+        // Verifica se a compilação deu certo
+
         let mut success = 0;
-        gl.GetShaderiv(vertex_shader,GL_COMPILE_STATUS,&mut success);
+        gl.GetShaderiv(vertex_shader, GL_COMPILE_STATUS, &mut success);
 
         if success == 0 {
             let mut v: Vec<u8> = Vec::with_capacity(1024); //Vetor que armazena o log de erro
             let mut log_len = 0_i32;
-            gl.GetShaderInfoLog(
-                vertex_shader,
-                1024,
-                &mut log_len,
-                v.as_mut_ptr().cast(),
-            );
+            gl.GetShaderInfoLog(vertex_shader, 1024, &mut log_len, v.as_mut_ptr().cast());
             v.set_len(log_len.try_into().unwrap());
-            panic!("Erro na compilação do vertex shader: {:?}", String::from_utf8_lossy(&v));
-       }
+            panic!(
+                "Erro na compilação do vertex shader: {:?}",
+                String::from_utf8_lossy(&v)
+            );
+        }
 
-    // Fragment Shader
+        // Fragment Shader
 
         let fragment_shader = gl.CreateShader(GL_FRAGMENT_SHADER);
-        assert_ne!(fragment_shader,0);
+        assert_ne!(fragment_shader, 0);
 
-    const FRAG_SHADER: &str = r#"#version 330 core
-    out vec4 final_color;
+        const FRAG_SHADER: &str = r#"
     uniform vec4 color;
     void main(){
         gl_FragColor = color;
     }
         "#;
 
-    // Compila o shader e verifica se a compilação deu certo
+        // Compila o shader e verifica se a compilação deu certo
         gl.ShaderSource(
             fragment_shader,
             1,
@@ -114,81 +113,192 @@ fn main() {
         if success == 0 {
             let mut v: Vec<u8> = Vec::with_capacity(1024); //Vetor que armazena o log de erro
             let mut log_len = 0_i32;
-            gl.GetShaderInfoLog(
-                fragment_shader,
-                1024,
-                &mut log_len,
-                v.as_mut_ptr().cast(),
-            );
+            gl.GetShaderInfoLog(fragment_shader, 1024, &mut log_len, v.as_mut_ptr().cast());
             v.set_len(log_len.try_into().unwrap());
-            panic!("Erro na compilação do fragment shader: {:?}", String::from_utf8_lossy(&v));
+            panic!(
+                "Erro na compilação do fragment shader: {:?}",
+                String::from_utf8_lossy(&v)
+            );
         }
-    // Cria um programa utilizando nossos shaders
+        // Cria um programa utilizando nossos shaders
         let shader_program = gl.CreateProgram();
         gl.AttachShader(shader_program, vertex_shader);
         gl.AttachShader(shader_program, fragment_shader);
         gl.LinkProgram(shader_program);
-        
+
         let mut success = 0;
         gl.GetProgramiv(shader_program, GL_LINK_STATUS, &mut success);
         if success == 0 {
             let mut v: Vec<u8> = Vec::with_capacity(1024);
             let mut log_len = 0_i32;
-            gl.GetProgramInfoLog(
-                shader_program,
-                1024,
-                &mut log_len,
-                v.as_mut_ptr().cast(),
-            );
+            gl.GetProgramInfoLog(shader_program, 1024, &mut log_len, v.as_mut_ptr().cast());
             v.set_len(log_len.try_into().unwrap());
-            panic!("Erro da linkagem dos shaders no programa: {}", String::from_utf8_lossy(&v));
+            panic!(
+                "Erro da linkagem dos shaders no programa: {}",
+                String::from_utf8_lossy(&v)
+            );
         }
-
-            // Dados pro vertex attribute pointer
-        let loc = gl.GetAttribLocation(shader_program,CString::new("position").unwrap().as_ptr() as *const u8);
+        gl.BindVertexArray(vao);
+        gl.UseProgram(shader_program);
+        // Dados pro vertex attribute pointer
+        let loc = gl.GetAttribLocation(
+            shader_program,
+            CString::new("position").unwrap().as_ptr() as *const u8,
+        );
         gl.EnableVertexAttribArray(loc.try_into().unwrap());
-        gl.VertexAttribPointer(loc.try_into().unwrap(),3,GL_FLOAT,0,size_of::<Vertex>().try_into().unwrap(),std::ptr::null(),);
+        gl.VertexAttribPointer(
+            loc.try_into().unwrap(),
+            3,
+            GL_FLOAT,
+            0,
+            size_of::<Vertex>().try_into().unwrap(),
+            0 as *const _,
+        );
 
-    // Agora que os shaders foram linkados, podemos deletar eles
+        // Agora que os shaders foram linkados, podemos deletar eles
         gl.DeleteShader(vertex_shader);
         gl.DeleteShader(fragment_shader);
-    // Vsync
-    glfw.set_swap_interval(glfw::SwapInterval::Sync(1_u32));
+        // Vsync
+        glfw.set_swap_interval(glfw::SwapInterval::Sync(1_u32));
 
-    // position
+        // position
 
-    
-    // Muda a cor do fundo
-    gl.ClearColor(0.3,0.3,0.3,1.0);
-    
-    //loop até usuário fechar a janela
-    while !window.should_close() {
+        // Muda a cor do fundo
+        gl.ClearColor(0.3, 0.3, 0.3, 1.0);
+
+        //Matriz de rotação
+        //loop até usuário fechar a janela
+        let mut radians = 0.0;
+        while !window.should_close() {
+            radians += 10.0;
+            let mut rng = rand::rng();
             gl.Clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            let loc_color = gl.GetUniformLocation(shader_program,CString::new("color").unwrap().as_ptr() as *const u8);
-            let loc = gl.GetUniformLocation(shader_program,CString::new("position").unwrap().as_ptr() as *const u8);
-            gl.UniformMatrix4fv(loc,1,1,IDENTITY_MATRIX.to_matrix4fv());
+            let loc_color = gl.GetUniformLocation(
+                shader_program,
+                CString::new("color").unwrap().as_ptr() as *const u8,
+            );
+            let loc = gl.GetUniformLocation(
+                shader_program,
+                CString::new("mat_transformation").unwrap().as_ptr() as *const u8,
+            );
+            let uma_matriz_rotacao_y = matriz_rotacao_y(radians * rng.random::<f32>());
+            let matriz_transformacao = IDENTITY_MATRIX
+                .multiplication(&uma_matriz_rotacao_y)
+                .multiplication(&matriz_rotacao_x(radians * rng.random::<f32>()));
+            gl.UniformMatrix4fv(
+                loc.try_into().unwrap(),
+                1,
+                1,
+                matriz_transformacao.to_matrix4fv().try_into().unwrap(),
+            );
+            let r: f32 = rng.random();
+            let g: f32 = rng.random();
+            let b: f32 = rng.random();
+
             for triangle in (0..vertices_cilindro.len()).step_by(3) {
-                gl.Uniform4f(loc_color.try_into().unwrap(),0.5,0.5,1.0,1.0);
-                gl.DrawArrays(GL_TRIANGLES,triangle.try_into().unwrap(),3);
+                // println!("{:?}",triangle);
+                println!("{r},{g},{b}");
+                gl.Uniform4f(loc_color.try_into().unwrap(), r, g, b, 1.0);
+                gl.DrawArrays(GL_TRIANGLES, triangle.try_into().unwrap(), 3);
             }
 
-        for (_, event) in glfw::flush_messages(&events) {
-            println!("{:?}",event);
-            if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
-                window.set_should_close(true)
+            for (_, event) in glfw::flush_messages(&events) {
+                println!("{:?}", event);
+                if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
+                    window.set_should_close(true)
+                }
             }
+            window.swap_buffers();
+            glfw.poll_events();
         }
-        window.swap_buffers();
-        glfw.poll_events();
-
-    }
     }
 }
-type Vertex = [f32;3];
-type V4Matrix = [[f32;4];4];
-const IDENTITY_MATRIX:V4Matrix = [[1.0,0.0,0.0,0.0], [0.0,1.0,0.0,0.0],[0.0,0.0,1.0,0.0],[0.0,0.0,0.0,1.0]];
+type Vertex = [f32; 3];
+type V4Matrix = [[f32; 4]; 4];
+const IDENTITY_MATRIX: V4Matrix = [
+    [1.0, 0.0, 0.0, 0.0],
+    [0.0, 1.0, 0.0, 0.0],
+    [0.0, 0.0, 1.0, 0.0],
+    [0.0, 0.0, 0.0, 1.0],
+];
 pub trait Matrix4fvTransformable {
     fn to_matrix4fv(&self) -> *const c_float;
+}
+
+pub trait V4MatrixUtils {
+    fn multiplication(&self, another_matrix: &V4Matrix) -> V4Matrix;
+}
+
+impl V4MatrixUtils for V4Matrix {
+    fn multiplication(&self, another_matrix: &V4Matrix) -> V4Matrix {
+        let mut result: V4Matrix = [[0.0; 4]; 4];
+        for i in 0..4 {
+            for j in 0..4 {
+                let mut soma: f32 = 0.0;
+                for k in 0..4 {
+                    soma += self[i][k] * another_matrix[k][j];
+                }
+                result[i][j] = soma;
+            }
+        }
+        return result;
+    }
+}
+
+fn matriz_rotacao_x(angulo: f32) -> V4Matrix {
+    [
+        [1.0, 0.0, 0.0, 0.0],
+        [
+            0.0,
+            angulo.to_radians().cos(),
+            -angulo.to_radians().sin(),
+            0.0,
+        ],
+        [
+            0.0,
+            angulo.to_radians().sin(),
+            angulo.to_radians().cos(),
+            0.0,
+        ],
+        [0.0, 0.0, 0.0, 1.0],
+    ]
+}
+
+fn matriz_rotacao_y(angulo: f32) -> V4Matrix {
+    [
+        [
+            angulo.to_radians().cos(),
+            0.0,
+            -angulo.to_radians().sin(),
+            0.0,
+        ],
+        [0.0, 1.0, 0.0, 0.0],
+        [
+            angulo.to_radians().sin(),
+            0.0,
+            angulo.to_radians().cos(),
+            0.0,
+        ],
+        [0.0, 0.0, 0.0, 1.0],
+    ]
+}
+fn matriz_rotacao_z(angulo: f32) -> V4Matrix {
+    [
+        [
+            angulo.to_radians().cos(),
+            -angulo.to_radians().sin(),
+            0.0,
+            0.0,
+        ],
+        [
+            angulo.to_radians().sin(),
+            angulo.to_radians().cos(),
+            0.0,
+            0.0,
+        ],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0],
+    ]
 }
 
 impl Matrix4fvTransformable for V4Matrix {
