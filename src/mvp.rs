22d6cea891355a85fb5fa328d317ae036e1f64ba
Gustavo-Blois <gustavobloisrr@gmail.com>
2025-04-18 22:11:17 -0300

    aa

--- src/mvp.rs
+++ src/mvp.rs
@@ -45,7 +45,6 @@ fn view(camera: Camera) -> V4Matrix {
    let [px, py, pz] = camera.camera_pos;
    let matriz_translacao = matriz_translacao(-px, -py, -pz);
    let [lx,ly,lz] = camera.camera_front;
-   let [upx, upy, upz] = camera.camera_up;
    let [izc,jzc,kzc] = normalize([px-lx,py-ly,pz-lz]);
    let [ixc,jxc,kxc] = normalize(produto_vetorial(camera.camera_up,[izc,jzc,kzc]));
    let [iyc,jyc,kyc] = produto_vetorial([izc,jzc,kzc],[ixc,jxc,kxc]);
@@ -60,8 +59,10 @@ fn view(camera: Camera) -> V4Matrix {
 }
 
 
-fn projection(fov:f32,aspect:f32,near:f32,far:f32) -> V4Matrix {
-    
-
-
+fn projection(near:f32,far:f32,top:f32,bottom:f32, right:f32, left:f32) -> V4Matrix {
+    [[2.0*near/right-left,0.0,right+left/right-left,0.0],
+    [0.0,2.0*near/top-bottom,top+bottom/top-bottom,0.0],
+    [0.0,0.0,(-1.0*(far+near))/far-near,(-2.0*far*near)/far-near],
+    [0.0,0.0,-1.0,0.0],
+    ]
 }
